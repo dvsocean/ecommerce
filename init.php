80f324f754bfcb3e887ced4834364129317307de
dvsocean <dvsocean@icloud.com>
2017-01-30 15:26:25 -0800

    initializes all files throughout the site

---
+++ init.php
@@ -0,0 +1,19 @@
+<?php 
+// ob_start();
+
+defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
+define('SITE_ROOT', DS . 'Applications' . DS . 'XAMPP' . DS . 'htdocs');
+define('INCLUDES_PATH', SITE_ROOT . DS . 'setup');
+
+require_once "new_config.php";
+require_once "database.php";
+require_once "object.php";
+require_once "comments_class.php";
+require_once "user.php";
+require_once "photo.php";
+require_once "records.php";
+require_once "session.php";
+require_once "helper.php";
+
+
+?>
